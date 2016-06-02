d7748ad9f226ea561db9b119a911c59858e3e5d3
Amitava saha <amitava@azuqua.com>
2016-06-02 17:28:36 +0530

    • new design ( http://www.lobdell.me/ohno ) • select all • New emoji-selectors (scrolling side lists) • Scrolling the new emoji-selectors "swap" current selections in real time. • Hide all scrollbars • Clear button X

--- components/Phrase/Phrase.js
+++ components/Phrase/Phrase.js
@@ -1,6 +1,21 @@
 import React from 'react';
 
 class Phrase extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {
+      tip: false
+    };
+
+    this.toggleTip = this.toggleTip.bind(this);
+    this.clear = this.clear.bind(this);
+    this._handleChange = this._handleChange.bind(this);
+    this._handleKeyDown = this._handleKeyDown.bind(this);
+    this._handlePaste = this._handlePaste.bind(this);
+  }
+
+
   _handleKeyDown(e) {
     var allowed = [8, 32, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90];
     if (allowed.indexOf(e.which) < 0) {
@@ -16,11 +31,30 @@ class Phrase extends React.Component {
     e.preventDefault();
   }
 
+  clear(){
+    this.props._setState({ 'phrase': '' });
+    this.refs.editor.focus();
+  }
+
+  toggleTip(){
+    this.setState({tip: !this.state.tip});
+  }
+
   render() {
+    const {phrase} = this.props;
     return (
       <div className="Phrase">
         <div className="field">
-          <input type="text" name="phrase" id="phrase" value={this.props.phrase} placeholder="Type a word or phrase" autoFocus onPaste={(e) => this._handlePaste(e)} onKeyDown={(e) => this._handleKeyDown(e)} onChange={(e) => this._handleChange(e)} />
+          <input type="text" name="phrase" id="phrase" autoFocus
+                 ref="editor"
+                 value={phrase}
+                 placeholder="TYPE HERE"
+                 onPaste={this._handlePaste}
+                 onKeyDown={this._handleKeyDown}
+                 onChange={this._handleChange} />
+          <button className="clear"
+                  onClick={this.clear} onMouseEnter={this.toggleTip} onMouseLeave={this.toggleTip}>&times;</button>
+          {phrase && this.state.tip ? <div className="tip">Clear input</div> : null}
         </div>
       </div>
     );
