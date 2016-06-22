40f5d770f1b39a76b91636e5ae216baa6a883bc9
Eric Lobdell <EricBenjamin@users.noreply.github.com>
2016-06-22 09:00:23 -0700

    Tried to resolve issue with sendfile

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/components/'));
 app.get("/", function(req, res) {
-   res.sendfile('app/app.html')
+   res.sendfile(__dirname+'/app.html')
 });
 
 // Run it
